f44544d6bfd041ab5f1bbea3b092df2c3346e099
cmldk <cemal.dak@gmail.com>
2019-11-28 15:59:12 +0300

    Initial commit

---
+++ lab7/MovieBrowser/app/src/main/java/com/example/moviebrowser/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.moviebrowser;
+
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity implements MovieFragment.OnMovieSelectionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void movieSelected(Movie item) {
+
+        int display_mode = getResources().getConfiguration().orientation;
+        if (display_mode == Configuration.ORIENTATION_PORTRAIT){
+            Intent intent = new Intent(MainActivity.this,DetailsActivity.class);
+            intent.putExtra("movie", item);
+            startActivity(intent);
+        }else{
+            DetailsFragment df = (DetailsFragment) getSupportFragmentManager().findFragmentByTag("details");
+            if (df == null){
+                FragmentTransaction fts= getSupportFragmentManager().beginTransaction();
+                df = DetailsFragment.newInstance(item);
+                fts.add(R.id.container,df,"details");
+                fts.commit();
+            }else{
+                df.setMovie(item,findViewById(R.id.container));
+            }
+        }
+
+    }
+}
